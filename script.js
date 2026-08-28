fee10be50e064c88788b2e47c16a694f46d95bb2
Casey Jones <cjone345@wgu.edu>
2026-08-27 20:05:07 -0400

    Set Blueprint Grid as default site design

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-const themeSelect = document.querySelector("#theme-select");
 const themeStylesheet = document.querySelector("#theme-stylesheet");
 
-if (themeSelect && themeStylesheet) {
+if (themeStylesheet) {
   const themePath = document.body.dataset.themePath || "";
-  const defaultTheme = "theme-toon-sketch.css";
-  const allowedThemes = Array.from(themeSelect.options).map((option) => option.value);
-
-  function normalizeThemeName(themeFile) {
-    if (!themeFile) {
-      return defaultTheme;
-    }
-
-    const fileName = themeFile.split("/").pop();
-    return allowedThemes.includes(fileName) ? fileName : defaultTheme;
-  }
-
-  function applyTheme(themeFile) {
-    const normalizedTheme = normalizeThemeName(themeFile);
-    themeStylesheet.setAttribute("href", themePath + normalizedTheme);
-    themeSelect.value = normalizedTheme;
-    localStorage.setItem("portfolio-theme", normalizedTheme);
-  }
-
-  const savedTheme = normalizeThemeName(localStorage.getItem("portfolio-theme"));
-  applyTheme(savedTheme);
-
-  themeSelect.addEventListener("change", function () {
-    applyTheme(themeSelect.value);
-  });
+  const defaultTheme = "theme-blueprint-grid.css";
+  themeStylesheet.setAttribute("href", themePath + defaultTheme);
 }
 
 const backToTopButton = document.querySelector(".back-to-top");
